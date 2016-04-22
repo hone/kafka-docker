1ba7b1d3cae27c6322719caa3faaf99ff91109c2
Terence Lee <hone02@gmail.com>
2016-04-22 17:35:46 -0400

    upgrade to docker-compose v2 format

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,10 @@ RUN mkdir -p /opt/ && \
   curl -s "http://mirror.metrocast.net/apache/kafka/0.9.0.1/kafka_2.11-0.9.0.1.tgz" | tar xzC /opt/
 ENV PATH $PATH:/opt/kafka_2.11-0.9.0.1/bin
 
-RUN mkdir -p /home/kafka
+RUN mkdir -p /home/kafka/bin/ /home/kafka/config/
+
+COPY bin/ /home/kafka/bin/
+COPY config/ /home/kafka/config/
+
+ENV HOME /home/kafka
+WORKDIR $HOME
--- bin/kafka-env-start.sh
+++ bin/kafka-env-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-SCALA_VERSION="2.10"
-KAFKA_VERSION="0.8.2.0"
+SCALA_VERSION="2.11"
+KAFKA_VERSION="0.9.0.1"
 CONFIG_PATH="/tmp/kafka/config/server.properties"
 IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 mkdir -p /tmp/kafka/config/
 cp /home/kafka/config/server.properties $CONFIG_PATH
-echo "zookeeper.connect=${ZOOKEEPER_PORT#tcp://}" >> $CONFIG_PATH
+echo "zookeeper.connect=zookeeper:2181" >> $CONFIG_PATH
 # hack around kafka advertising the docker CID as a valid broker
 echo "advertised.host.name=$IP_ADDRESS" >> $CONFIG_PATH
 kafka-server-start.sh $CONFIG_PATH
